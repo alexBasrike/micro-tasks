bb6c7875a499acc9bd612a1a44c4ed0e979d3e6d
Alexander Dmitriev <alex.basrike@gmail.com>
2023-08-20 15:26:28 +0300

    worked with useState

--- src/site/Body.tsx
+++ src/site/Body.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import {Cars, topCars} from "./Cars";
 import {Button} from "../components/Button";
+import {UseState} from "../components/UseState";
 
 type BodyPropsType = {
     titleForBody: string
@@ -8,6 +9,7 @@ type BodyPropsType = {
 
 export const Body = (props: BodyPropsType) => {
 
+    /*--- Button ---*/
     const Button1Foo = (subscriber: string, age: number, address: string) => {
         console.log(subscriber, age, address);
     }
@@ -19,16 +21,21 @@ export const Body = (props: BodyPropsType) => {
     const Button3Foo = () => {
         console.log("I'm stupid button");
     }
+    /*--- Button ---*/
 
     return (
 
         <main>
             {props.titleForBody}
             <Cars topCars={topCars}/>
-            {/*<button onClick={ (event) => { console.log('Hello!') } }>MyYouTubeChanel-1</button>*/}
-            <Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("I'm Vasya", 21, "live in Minsk")}/>
-            <Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("I'm Ivan")}/>
-            <Button name={"Stupid BUTTON"} callBack={Button3Foo}/>
+
+            <div>
+                <Button name={"MyYouTubeChanel-1"} callBack={() => Button1Foo("I'm Vasya", 21, "live in Minsk")}/>
+                <Button name={"MyYouTubeChanel-2"} callBack={() => Button2Foo("I'm Ivan")}/>
+                <Button name={"Stupid BUTTON"} callBack={Button3Foo}/>
+            </div>
+
+            <UseState/>
         </main>
     );
 };
