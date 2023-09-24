be4cc7d5461d30c99c8999b535cbd47df42379bc
Alexander Dmitriev <alex.basrike@gmail.com>
2023-09-24 15:02:38 +0300

    added new branch for Counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import {Body} from "./site/Body";
 
 function App() {
     return (
         <div className="App">
-            <Body titleForBody={"TodoList"}/>
+
         </div>
     );
 }
